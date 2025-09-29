51bd45a186dd39a286133055c2315a24fc6e3390
Divine7205 <adegokedivine1@gmail.com>
2025-09-29 22:25:18 +0100

    fixed synchronous issues

--- app/src/main/java/com/practice/weatherapp/MainActivity.java
+++ app/src/main/java/com/practice/weatherapp/MainActivity.java
@@ -37,8 +37,8 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
     private String API = "14e719695ce420f98a14dea8163654ab";
 
-    private double latitude = -0.1257;
-    private double longitude = 0.0;
+    private double latitude = 51.5085;
+    private double longitude = -0.1257;
 
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
     private final Handler handler = new Handler(Looper.getMainLooper());
@@ -65,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize UI variables
+
         loader = findViewById(R.id.loader);
         mainContainer = findViewById(R.id.mainContainer);
         error = findViewById(R.id.error);
@@ -81,31 +84,48 @@ public class MainActivity extends AppCompatActivity {
         humidity = findViewById(R.id.humidity);
         ImageButton refresh = findViewById(R.id.refresh);
         locationProvider = LocationServices.getFusedLocationProviderClient(this);
-
-        fetchWeather();
-        refresh.setOnClickListener(v -> fetchWeather());
+        // Call functions to fetch weather
+        initiateWeatherFetch();
+        refresh.setOnClickListener(v -> initiateWeatherFetch());
 
     }
 
+    private void initiateWeatherFetch() {
+        // Initiate Processes
+        progressing();
+        getCurrentLocation();
+    }
 
+    @SuppressLint("MissingPermission")
     private void getCurrentLocation(){
-        if (checkLocation()){
+        if (checkPermissions()){
+            // Permission is granted
             if (isLocationEnabled()){
+                // Location is enabled
                 locationProvider.getLastLocation().addOnCompleteListener(task -> {
+                    // Get the location
                     Location location = task.getResult();
                     if (location != null){
                         latitude = location.getLatitude();
                         longitude = location.getLongitude();
+                        // Fetch the weather
+                        fetchWeather();
                     }
+                }).addOnFailureListener(e -> {
+                    // Handle failure
+                    Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+                    fetchWeather();
                 });
 
-            }else {
+            }
+            else {
+                // Location is not enabled
                 Toast.makeText(this, "Please turn on your location", Toast.LENGTH_LONG).show();
-                longitude = -0.1257;
-                latitude = 51.5085;
+                fetchWeather();
             }
 
-        }else {
+        }
+        else {
             requestPermission();
         }
     }
@@ -116,7 +136,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final int PERMISSION_REQUEST_ACCESS_CODE = 100;
 
-    private Boolean checkLocation(){
+    private Boolean checkPermissions(){
+        // Check if permissions are granted
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             return true;
         }
@@ -124,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Boolean isLocationEnabled(){
+        // Check if location is enabled
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
             return true;
@@ -136,14 +158,17 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
         if (requestCode == PERMISSION_REQUEST_ACCESS_CODE){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                fetchWeather();
+                // Permission granted
+                initiateWeatherFetch();
             }else{
-                longitude = -0.1257;
-                latitude = 51.5085;
+                // Permission denied
+                Toast.makeText(this, "Location permission denied. Showing weather for default location.", Toast.LENGTH_LONG).show();
+                fetchWeather();
             }
         }
     }
 
+    // Screens states
     private void progressing(){
         loader.setVisibility(View.VISIBLE);
         mainContainer.setVisibility(View.GONE);
@@ -154,15 +179,14 @@ public class MainActivity extends AppCompatActivity {
         mainContainer.setVisibility(View.GONE);
         error.setVisibility(View.VISIBLE);
     }
-
     private void loaded(){
         loader.setVisibility(View.GONE);
         mainContainer.setVisibility(View.VISIBLE);
         error.setVisibility(View.GONE);
     }
+
     // Fetching data from the API
     private String getData(){
-        getCurrentLocation();
         //holds the result
         String result =null;
         // HTTP connection
@@ -175,7 +199,6 @@ public class MainActivity extends AppCompatActivity {
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
-            System.out.println("Connected");
 
             //Reading the data from the stream
             inputStream = connection.getInputStream();
@@ -212,26 +235,20 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
     private void fetchWeather() {
-        if (loader != null){
-            progressing();
-        }
+        // Run the fetch in a separate thread
         executorService.execute(() -> {
             // Post the result to the main thread
             final String finalResult = getData();
             handler.post(() -> {
                 if (!Objects.equals(finalResult, "Error")){
                     try {
+                        // Parsing the data
                         JSONObject jsonObject = new JSONObject(finalResult);
                         JSONObject weather = jsonObject.getJSONArray("weather").getJSONObject(0);
-                        System.out.println(1);
                         JSONObject main = jsonObject.getJSONObject("main");
-                        System.out.println(2);
                         JSONObject sys = jsonObject.getJSONObject("sys");
-                        System.out.println(3);
                         JSONObject winds = jsonObject.getJSONObject("wind");
-                        System.out.println(4);
                         long updatedAt = jsonObject.getLong("dt");
-                        System.out.println(5);
                         //Assigning values to variables
                         // Date and time
                         String updatedAtText = "Updated at: " + new java.text.SimpleDateFormat("dd/MM/yyyy hh:mm a", java.util.Locale.ENGLISH).format(new java.util.Date(updatedAt * 1000));
@@ -240,7 +257,7 @@ public class MainActivity extends AppCompatActivity {
                         String sunriseText = new java.text.SimpleDateFormat("hh:mm a", java.util.Locale.ENGLISH).format(new java.util.Date(sunriseNo * 1000));
                         String sunsetText = new java.text.SimpleDateFormat("hh:mm a", java.util.Locale.ENGLISH).format(new java.util.Date(sunsetNo * 1000));
 
-                        // Temperature
+                        // Temperatures
                         String temperature = main.getString("temp") + "°C";
                         String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
                         String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
@@ -265,13 +282,13 @@ public class MainActivity extends AppCompatActivity {
                         humidity.setText(humidityText);
                         address.setText(addressText);
                         loaded();
-                    } catch (Exception e){
-                        e.printStackTrace();
-//                        showError();
-                        loaded();
-
                     }
-                } else {
+                    catch (Exception e){
+                        e.printStackTrace();
+                        showError();
+                    }
+                }
+                else {
                     showError();
                 }
             });
@@ -281,6 +298,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // Shutdown the executor service
         if (executorService != null && !executorService.isShutdown())
             executorService.shutdown();
     }
